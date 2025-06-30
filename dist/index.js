a2c23d5335cff6535d8f83e0c8361ec49bec99b3
郅鹏 <18830289852@163.com>
2025-06-30 23:20:08 +0800

    fix:解决当前npm后找不到导出的方法问题

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.mitt = factory());
+})(this, (function () { 'use strict';
+
+  class mitt {
+      
+    constructor() {
+      this.events = new Map();
+    }
+    // 触发事件
+    on(type, listener) {
+      this.events.has(type) ? this.events.get(type).push(listener) : this.events.set(type, [listener]);
+    }
+    // 注册事件
+    emit(type, ...args) {
+      this.events.get(type)?.map(fun => fun(...args));
+    }
+    off(type, listener) {
+      this.events.delete(type);
+    }
+    once() {
+
+    }
+    clear() {
+      this.events.clear();
+    }
+
+  }
+
+  return mitt;
+
+}));
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ export default {
   input: 'src/index.js',        // 入口文件
   output: {
     file: 'dist/index.js', // 输出路径
-    name: 'mitt',         // 全局变量名，在浏览器中可以通过 window.Broadcastor 访问
+    name: 'mitt',         // 全局变量名，在浏览器中可以通过 window.mitt 访问
     format: 'umd',               // 支持多种模块格式（AMD / CommonJS / 浏览器全局）
     sourcemap: false              // 是否生成 source map
   }
